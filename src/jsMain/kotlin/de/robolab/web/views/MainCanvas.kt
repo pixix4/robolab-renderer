f9ce5e8aee8e087fc858644fb080fae6a20cccf7
Lars Westermann <lars-westermann@live.de>
2020-04-16 09:52:36 +0200

    Add side bar resize view to js

--- src/jsMain/kotlin/de/robolab/web/views/MainCanvas.kt
+++ src/jsMain/kotlin/de/robolab/web/views/MainCanvas.kt
@@ -1,17 +1,21 @@
 package de.robolab.web.views
 
 import de.robolab.app.controller.CanvasController
-import de.robolab.web.adapter.WebCanvas
+import de.robolab.renderer.data.Point
+import de.robolab.web.adapter.*
+import de.westermann.kobserve.Property
 import de.westermann.kobserve.ReadOnlyProperty
 import de.westermann.kwebview.View
 import de.westermann.kwebview.ViewCollection
 import de.westermann.kwebview.components.Canvas
+import kotlin.browser.document
 import kotlin.browser.window
+import kotlin.math.max
 
 class MainCanvas(
         canvasController: CanvasController,
-        sideBarActiveProperty: ReadOnlyProperty<Boolean>,
-        infoBarActiveProperty: ReadOnlyProperty<Boolean>
+        sideBarActiveProperty: Property<Boolean>,
+        infoBarActiveProperty: Property<Boolean>
 ) : ViewCollection<View>() {
 
     private val canvas = Canvas()
@@ -36,8 +40,67 @@ class MainCanvas(
 
         +canvas
 
+        +ResizeView("side-bar-resize") { position, _ ->
+            var width = position.x
+            if (sideBarActiveProperty.value) {
+                if (width < 50.0) {
+                    sideBarActiveProperty.value = false
+                } else {
+                    width = max(width, 200.0)
+                    document.body?.style?.setProperty("--side-bar-width", "${width}px")
+                }
+            } else {
+                if (width >= 50.0) {
+                    sideBarActiveProperty.value = true
+                }
+            }
+        }
+
+        +ResizeView("info-bar-resize") { position, size ->
+            var width = window.innerWidth - position.x - size.x
+            if (infoBarActiveProperty.value) {
+                if (width < 50.0) {
+                    infoBarActiveProperty.value = false
+                } else {
+                    width = max(width, 200.0)
+                    document.body?.style?.setProperty("--info-bar-width", "${width}px")
+                }
+            } else {
+                if (width >= 50.0) {
+                    infoBarActiveProperty.value = true
+                }
+            }
+        }
+
         canvasController.setupCanvas(webCanvas)
 
         checkSizeChange(0.0)
+    }
+}
+
+class ResizeView(vararg cssClasses: String, onResize: (position: Point, size: Point) -> Unit) : View() {
+
+    private val hammer = Hammer(html, object {})
+
+    private var offset: Point = Point.ZERO
+
+    init {
+        for (cssClass in cssClasses) {
+            classList.add(cssClass)
+        }
+
+        hammer.enablePan()
+
+        hammer.onPanStart { event ->
+            val source = Point(offsetLeftTotal, offsetTopTotal)
+            val center = event.center.let { Point(it.x, it.y) }
+            offset = center - source
+        }
+
+        hammer.onPanMove { event ->
+            val center = event.center.let { Point(it.x, it.y) }
+            val size = Point(clientWidth, clientHeight)
+            onResize(center - offset, size)
+        }
     }
 }
