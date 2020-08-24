594a799c3267ce9ecc65fc305823f30d2ef7da34
Zincfox <alexander_C._schwerin@web.de>
2020-08-24 16:01:19 +0200

    Make ServerPlanetInfo great (not serializable) again
    Fixes bugf

--- src/commonMain/kotlin/de/robolab/common/planet/PlanetInfo.kt
+++ src/commonMain/kotlin/de/robolab/common/planet/PlanetInfo.kt
@@ -16,11 +16,9 @@ interface IPlanetInfo<IDType> {
 }
 
 
-@Serializable
 data class ServerPlanetInfo(
     override val id: String,
     override val name: String,
-    @SerialName("lastModified")
     val lastModifiedString: String
 ) : IPlanetInfo<String> {
 
