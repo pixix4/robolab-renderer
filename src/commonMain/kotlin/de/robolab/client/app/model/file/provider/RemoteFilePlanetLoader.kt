6bf9260ebb86803a34d70752b43386e5f9577bec
Lars Westermann <lars-westermann@live.de>
2020-08-28 15:48:24 +0200

    Fix error message on missing remote source

--- src/commonMain/kotlin/de/robolab/client/app/model/file/provider/RemoteFilePlanetLoader.kt
+++ src/commonMain/kotlin/de/robolab/client/app/model/file/provider/RemoteFilePlanetLoader.kt
@@ -3,12 +3,12 @@ package de.robolab.client.app.model.file.provider
 import de.robolab.client.app.model.base.MaterialIcon
 import de.robolab.client.net.ICredentialProvider
 import de.robolab.client.net.IRobolabServer
-import de.robolab.client.net.requests.PlanetJsonInfo
 import de.robolab.client.net.RESTRobolabServer
 import de.robolab.client.net.requests.*
 import de.robolab.common.net.HttpStatusCode
 import de.westermann.kobserve.event.EventHandler
 import de.westermann.kobserve.property.constObservable
+import de.westermann.kobserve.property.property
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -24,42 +24,77 @@ class RemoteFilePlanetLoader(
 
     override val iconProperty = constObservable(MaterialIcon.CLOUD_QUEUE)
 
-    override val availableProperty = constObservable(true)
+    override val availableProperty = property(true)
+    var available by availableProperty
 
     override suspend fun loadPlanet(identifier: PlanetJsonInfo): Pair<PlanetJsonInfo, List<String>>? {
         return withContext(Dispatchers.Default) {
-            val result = server.getPlanet(identifier.id)
-            if (result.status != HttpStatusCode.Ok) {
+            try {
+                val result = server.getPlanet(identifier.id)
+                if (result.status != HttpStatusCode.Ok) {
+                    available = false
+                    null
+                } else {
+                    available = true
+                    PlanetJsonInfo(identifier.id, identifier.name, result.lastModified, identifier.tags) to result.lines
+                }
+            } catch (e: Exception) {
+                available = false
                 null
-            } else {
-                PlanetJsonInfo(identifier.id, identifier.name, result.lastModified, identifier.tags) to result.lines
             }
         }
     }
 
     override suspend fun savePlanet(identifier: PlanetJsonInfo, lines: List<String>): PlanetJsonInfo? {
         return withContext(Dispatchers.Default) {
-            val result = server.putPlanet(identifier.id, lines.joinToString("\n"))
+            try {
+                val result = server.putPlanet(identifier.id, lines.joinToString("\n"))
 
-            if (result.status != HttpStatusCode.Ok) null else identifier
+                if (result.status != HttpStatusCode.Ok) {
+                    available = false
+                    null
+                } else {
+                    available = true
+                    identifier
+                }
+            } catch (e: Exception) {
+                available = false
+                null
+            }
         }
     }
 
     override suspend fun createPlanet(lines: List<String>) {
         return withContext(Dispatchers.Default) {
-            server.postPlanet(lines.joinToString("\n"))
+            try {
+                available = true
+                server.postPlanet(lines.joinToString("\n"))
+            } catch (e: Exception) {
+                available = false
+            }
         }
     }
 
     override suspend fun deletePlanet(identifier: PlanetJsonInfo) {
         return withContext(Dispatchers.Default) {
-            server.deletePlanet(identifier.id)
+            try {
+                available = true
+                server.deletePlanet(identifier.id)
+            } catch (e: Exception) {
+                available = false
+            }
         }
     }
 
     override suspend fun listPlanets(identifier: PlanetJsonInfo?): List<PlanetJsonInfo> {
         return withContext(Dispatchers.Default) {
-            server.listPlanets().planets
+            try {
+                available = true
+                server.listPlanets().planets
+            } catch (e: Exception) {
+                available = false
+                emptyList()
+            }
         }
     }
 
