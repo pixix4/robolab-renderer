pixix4/robolab-renderer

commit f131beb3e178b2c58b1decb25cd2977563f04fec

Add GetMQTTCredentials

src/jsServerMain/kotlin/de/robolab/server/auth/User.kt
@@ -1,29 +1,31 @@
 package de.robolab.server.auth
 
+import de.robolab.common.net.RESTRequestError
 import de.robolab.server.externaljs.dynamicOf
 
 typealias UserID = UInt
-class User(val userID: UserID, val isRoboLab: Boolean) {
-    fun toJWTPayload():dynamic = dynamicOf(
+
+class User(val userID: UserID, val roboLvl: Int) {
+    fun toJWTPayload(): dynamic = dynamicOf(
         "sub" to userID.toString(),
-        "roboLvl" to if(isRoboLab) 10 else 0
+        "roboLvl" to roboLvl
     )
 
-    fun toJSON(): dynamic{
+    fun toJSON(): dynamic {
         val payload: dynamic = toJWTPayload()
         payload.anonymous = this == Anonymous
         return payload
     }
 
     val internalName: String
-        get() = if(this == Anonymous) "anonymous" else userID.toString()
+        get() = if (this == Anonymous) "anonymous" else userID.toString()
 
-    companion object{
-        val Anonymous: User = User(UserID.MAX_VALUE,false)
-        fun fromJWTPayload(payload: dynamic): User?{
+    companion object {
+        val Anonymous: User = User(UserID.MAX_VALUE, Int.MIN_VALUE)
+        fun fromJWTPayload(payload: dynamic): User? {
             val sub = payload.sub as? String ?: return null
             val roboLvl = payload.roboLvl as? Int ?: return null
-            return User(sub.toUInt(),roboLvl>=10)
+            return User(sub.toUInt(), roboLvl)
         }
     }
 
@@ -35,3 +37,54 @@ class User(val userID: UserID, val isRoboLab: Boolean) {
         return userID.hashCode()
     }
 }
+
+abstract class UserPermissionException : RESTRequestError {
+    val user: User
+
+    constructor(
+        user: User,
+        message: String = "The user '${user.internalName}' does not have the required permissions"
+    ) : super(message) {
+        this.user = user
+    }
+
+    constructor(
+        user: User,
+        message: String = "The user '${user.internalName}' does not have the required permissions",
+        cause: Throwable?
+    ) : super(message, cause) {
+        this.user = user
+    }
+}
+
+class UserNumericPermissionException : UserPermissionException {
+    val requiredLevel: Int
+
+
+    constructor(
+        user: User,
+        requiredLevel: Int,
+        message: String = "The user '${user.internalName}' does not have the required permission-Level of $requiredLevel, only ${user.roboLvl}"
+    ) : super(user, message) {
+        this.requiredLevel = requiredLevel
+    }
+
+    constructor(
+        user: User,
+        requiredLevel: Int,
+        message: String = "The user '${user.internalName}' does not have the required permission-Level of $requiredLevel, only ${user.roboLvl}",
+        cause: Throwable?
+    ) : super(user, message, cause) {
+        this.requiredLevel = requiredLevel
+    }
+}
+
+fun User.requireLogin(userLevel: Int) {
+    if (userLevel < roboLvl) throw UserNumericPermissionException(this, userLevel)
+}
+
+fun User.requireLogin() = requireLogin(0)
+
+fun User.requireTutor() = requireLogin(10)
+
+fun User.requireAdmin() = requireLogin(40)

server.ini
@@ -13,3 +13,8 @@ examPlanetSmallName = ExminatorSmall
 [planets]
 directory = ../robolab-planets/live/
 database = redis://127.0.0.1:6379/4
+
+[mqtt]
+tutorUser = tutor
+tutorPassword = password
+

commit efff12af15907f6864c2051a4a19678b8e6661eb

Separate RESTRequestException and RESTResponseException
Also fix the bug introduced in f131beb3e178b2c58b1decb25cd2977563f04fec which inverted the permission-tests.

src/commonMain/kotlin/de/robolab/client/net/requests/GetToken.kt
@@ -27,10 +27,10 @@ object GetTokenLinkPair : IUnboundRESTRequest<GetTokenLinkPair.TokenLinkResponse
             if (status != HttpStatusCode.Ok)
                 `throw`(triggeringRequest)
             val r = this.parse(R.serializer())
-                ?: throw RESTRequestError("Could not parse body", triggeringRequest, this)
+                ?: throw RESTRequestException("Could not parse body", triggeringRequest, this)
             tokenURL = r.token
             loginURL = r.login
-            requestURL = (URLInfo.fromURL(tokenURL)?:throw RESTRequestError())
+            requestURL = (URLInfo.fromURL(tokenURL)?:throw RESTRequestException())
         }
 
 
@@ -52,13 +52,13 @@ object GetTokenLinkPair : IUnboundRESTRequest<GetTokenLinkPair.TokenLinkResponse
                 if (status != HttpStatusCode.Ok)
                     `throw`(triggeringRequest)
                 if (contentType?.mimeType != MIMEType.JWT) {
-                    throw RESTRequestError(
+                    throw RESTRequestException(
                         "Cannot parse MIME-Type \"${contentType?.mimeType}\"",
                         triggeringRequest,
                         this
                     )
                 }
-                rawToken = body ?: throw RESTRequestError("Token response does not have a body", triggeringRequest, this)
+                rawToken = body ?: throw RESTRequestException("Token response does not have a body", triggeringRequest, this)
             }
 
             val tokenHeader: AuthorizationHeader.Bearer by lazy {

src/commonMain/kotlin/de/robolab/client/net/requests/RESTResponse.kt
@@ -2,7 +2,7 @@ package de.robolab.client.net.requests
 
 import com.soywiz.klock.DateTime
 import de.robolab.client.net.IServerResponse
-import de.robolab.common.net.RESTRequestError
+import de.robolab.common.net.RESTRequestException
 import de.robolab.common.net.`throw`
 import de.robolab.client.net.contentType
 import de.robolab.common.net.HttpMethod
@@ -17,7 +17,7 @@ import kotlinx.serialization.builtins.serializer
 
 interface IRESTResponse : IServerResponse
 
-typealias RESTResult<T> = Result<T, RESTRequestError>
+typealias RESTResult<T> = Result<T, RESTRequestException>
 
 private const val EAGER_PLANET_EVAL = false
 

src/commonMain/kotlin/de/robolab/common/net/RESTRequestException.kt
@@ -3,13 +3,12 @@ package de.robolab.common.net
 import de.robolab.client.net.IServerResponse
 import de.robolab.client.net.ServerResponse
 import de.robolab.client.net.requests.IRESTRequest
-import de.robolab.client.net.requests.IUnboundRESTRequest
 import de.robolab.client.net.requests.IRESTResponse
 import de.robolab.client.net.requests.RESTResult
 import de.robolab.common.utils.Err
 import de.robolab.common.utils.Ok
 
-open class RESTRequestError : RuntimeException {
+open class RESTRequestException : RuntimeException {
     val triggeringRequest: IRESTRequest<*>?
     val triggeringResponse: IServerResponse?
 
@@ -50,7 +49,7 @@ open class RESTRequestError : RuntimeException {
     }
 }
 
-open class RESTRequestCodeError : RESTRequestError {
+open class RESTRequestCodeException : RESTRequestException {
     val code: HttpStatusCode
     val mimeType: MIMEType?
 
@@ -108,10 +107,10 @@ inline fun <R : IRESTResponse> parseResponseCatchingWrapper(
 ): RESTResult<R> = try {
     Ok(parser(response))
 } catch (ex: Exception) {
-    if(ex is RESTRequestError)
+    if(ex is RESTRequestException)
         Err(ex)
     else
-        Err(RESTRequestError(ex,triggeringRequest,response))
+        Err(RESTRequestException(ex,triggeringRequest,response))
 }
 
 inline fun <R : IRESTResponse, T: IRESTRequest<R>> parseResponseCatchingWrapper(
@@ -125,7 +124,7 @@ fun IServerResponse.`throw`(
 ): Nothing {
     val body = this.body
     if (body != null)
-        throw RESTRequestCodeError(status, body,triggeringResponse = this, triggeringRequest = triggeringRequest)
+        throw RESTRequestCodeException(status, body,triggeringResponse = this, triggeringRequest = triggeringRequest)
     else
-        throw RESTRequestCodeError(status, triggeringResponse = this, triggeringRequest = triggeringRequest)
+        throw RESTRequestCodeException(status, triggeringResponse = this, triggeringRequest = triggeringRequest)
 }

src/jsClientMain/kotlin/de/robolab/client/ui/dialog/TokenDialog.kt
@@ -4,13 +4,12 @@ import de.robolab.client.net.IRobolabServer
 import de.robolab.client.net.requests.getTokenLinkPair
 import de.robolab.client.net.sendHttpRequest
 import de.robolab.common.net.HttpStatusCode
-import de.robolab.common.net.RESTRequestCodeError
+import de.robolab.common.net.RESTRequestCodeException
 import de.robolab.common.utils.Logger
 import de.westermann.kobserve.event.emit
 import de.westermann.kwebview.components.button
 import de.westermann.kwebview.components.link
 import de.westermann.kwebview.components.textView
-import kotlinx.browser.window
 import kotlinx.coroutines.*
 
 class TokenDialog private constructor(
@@ -55,7 +54,7 @@ class TokenDialog private constructor(
                 while (isOpen) {
                     val r = tokenLinkPair.sendHttpRequest().also {
                         it.ifErr { e ->
-                            if (e !is RESTRequestCodeError || e.code != HttpStatusCode.NoContent) {
+                            if (e !is RESTRequestCodeException || e.code != HttpStatusCode.NoContent) {
                                 logger.warn(e.toString())
                             }
                         }

src/jsServerMain/kotlin/de/robolab/server/auth/AuthService.kt
@@ -4,7 +4,7 @@ import com.soywiz.klock.DateTime
 import com.soywiz.klock.TimeSpan
 import de.robolab.common.net.HttpStatusCode
 import de.robolab.common.net.headers.AuthorizationHeader
-import de.robolab.server.net.RESTRequestClientCodeError
+import de.robolab.server.net.RESTResponseCodeException
 import de.robolab.server.config.Config
 import de.robolab.server.externaljs.jsonwebtoken.JSONWebToken
 import de.robolab.server.externaljs.jsonwebtoken.parseJWT
@@ -74,7 +74,7 @@ class AuthService(val random: Random = Random, val shareCodeTimeout:TimeSpan=Tim
 
     fun assertCodeExists(code: ShareCode): Unit {
         if (code !in activeShareCodes)
-            throw RESTRequestClientCodeError(HttpStatusCode.NotFound, "Code does not exist")
+            throw RESTResponseCodeException(HttpStatusCode.NotFound, "Code does not exist")
     }
 
     fun assertCanProvide(code: ShareCode): Unit {
@@ -83,7 +83,7 @@ class AuthService(val random: Random = Random, val shareCodeTimeout:TimeSpan=Tim
             if (code !in providedShares)
                 return
             else
-                throw RESTRequestClientCodeError(HttpStatusCode.BadRequest, "Code is already providing a Token")
+                throw RESTResponseCodeException(HttpStatusCode.BadRequest, "Code is already providing a Token")
     }
 
     // Returns true if the ShareCode is used in a share-process, false otherwise
@@ -101,7 +101,7 @@ class AuthService(val random: Random = Random, val shareCodeTimeout:TimeSpan=Tim
             return false
         }
         if (providedShares.containsKey(code))
-            throw RESTRequestClientCodeError(HttpStatusCode.BadRequest, "Code is already providing a Token")
+            throw RESTResponseCodeException(HttpStatusCode.BadRequest, "Code is already providing a Token")
         providedShares[code] = userID to token
         return true
     }
@@ -117,7 +117,7 @@ class AuthService(val random: Random = Random, val shareCodeTimeout:TimeSpan=Tim
             activeShareCodes.remove(code)
             return providedValue.second
         } else
-            throw RESTRequestClientCodeError(
+            throw RESTResponseCodeException(
                 HttpStatusCode.Forbidden,
                 "Requesting UserID does not match Providing UserID"
             )

src/jsServerMain/kotlin/de/robolab/server/auth/GitLabAuthProvider.kt
@@ -71,7 +71,7 @@ class GitLabAuthProvider(private val callbackURL: String) {
         }
         val robolabUserSet = getRoboLabUserSet()
         val currentUserId = currentUserResponse.jsonBody!!.jsonObject["resource_owner_id"]!!.jsonPrimitive.int.toUInt()
-        return User(currentUserId, currentUserId in robolabUserSet)
+        return User(currentUserId, if(currentUserId in robolabUserSet) 10 else 0)
     }
 
     fun extractShareCode(state: String): ShareCode? {

src/jsServerMain/kotlin/de/robolab/server/auth/User.kt
@@ -1,14 +1,15 @@
 package de.robolab.server.auth
 
-import de.robolab.common.net.RESTRequestError
+import de.robolab.common.net.HttpStatusCode
 import de.robolab.server.externaljs.dynamicOf
+import de.robolab.server.net.RESTResponseCodeException
 
 typealias UserID = UInt
 
-class User(val userID: UserID, val roboLvl: Int) {
+class User(val userID: UserID, val accessLevel: Int) {
     fun toJWTPayload(): dynamic = dynamicOf(
         "sub" to userID.toString(),
-        "roboLvl" to roboLvl
+        "accessLevel" to accessLevel
     )
 
     fun toJSON(): dynamic {
@@ -24,8 +25,8 @@ class User(val userID: UserID, val roboLvl: Int) {
         val Anonymous: User = User(UserID.MAX_VALUE, Int.MIN_VALUE)
         fun fromJWTPayload(payload: dynamic): User? {
             val sub = payload.sub as? String ?: return null
-            val roboLvl = payload.roboLvl as? Int ?: return null
-            return User(sub.toUInt(), roboLvl)
+            val accessLevel = payload.accessLevel as? Int ?: return null
+            return User(sub.toUInt(), accessLevel)
         }
     }
 
@@ -38,23 +39,40 @@ class User(val userID: UserID, val roboLvl: Int) {
     }
 }
 
-abstract class UserPermissionException : RESTRequestError {
+abstract class UserPermissionException : RESTResponseCodeException {
     val user: User
 
     constructor(
         user: User,
-        message: String = "The user '${user.internalName}' does not have the required permissions"
-    ) : super(message) {
+        message: String = "The user '${user.internalName}' does not have the required permissions",
+        statusCode: HttpStatusCode = HttpStatusCode.Forbidden
+    ) : super(statusCode, message) {
         this.user = user
     }
 
     constructor(
         user: User,
         message: String = "The user '${user.internalName}' does not have the required permissions",
-        cause: Throwable?
-    ) : super(message, cause) {
+        cause: Throwable?,
+        statusCode: HttpStatusCode = HttpStatusCode.Forbidden
+    ) : super(statusCode, message, cause = cause) {
         this.user = user
     }
+}
+
+class UserUnauthorizedException : UserPermissionException {
+    constructor(message: String = "Authorization required") : super(
+        User.Anonymous,
+        message,
+        HttpStatusCode.Unauthorized
+    )
+
+    constructor(message: String = "Authorization required", cause: Throwable?) : super(
+        User.Anonymous,
+        message,
+        cause,
+        HttpStatusCode.Unauthorized
+    )
 }
 
 class UserNumericPermissionException : UserPermissionException {
@@ -64,7 +82,7 @@ class UserNumericPermissionException : UserPermissionException {
     constructor(
         user: User,
         requiredLevel: Int,
-        message: String = "The user '${user.internalName}' does not have the required permission-Level of $requiredLevel, only ${user.roboLvl}"
+        message: String = "The user '${user.internalName}' does not have the required permission-Level of $requiredLevel, only ${user.accessLevel}"
     ) : super(user, message) {
         this.requiredLevel = requiredLevel
     }
@@ -72,15 +90,16 @@ class UserNumericPermissionException : UserPermissionException {
     constructor(
         user: User,
         requiredLevel: Int,
-        message: String = "The user '${user.internalName}' does not have the required permission-Level of $requiredLevel, only ${user.roboLvl}",
+        message: String = "The user '${user.internalName}' does not have the required permission-Level of $requiredLevel, only ${user.accessLevel}",
         cause: Throwable?
     ) : super(user, message, cause) {
         this.requiredLevel = requiredLevel
     }
 }
 
-fun User.requireLogin(userLevel: Int) {
-    if (userLevel < roboLvl) throw UserNumericPermissionException(this, userLevel)
+fun User.requireLogin(accessLevel: Int) {
+    if (this == User.Anonymous) throw UserUnauthorizedException()
+    if (this.accessLevel < accessLevel) throw UserNumericPermissionException(this, accessLevel)
 }
 
 fun User.requireLogin() = requireLogin(0)

src/jsServerMain/kotlin/de/robolab/server/data/FilePlanetStore.kt
@@ -4,7 +4,7 @@ import com.soywiz.klock.js.toDateTime
 import de.robolab.common.net.HttpStatusCode
 import de.robolab.common.parser.PlanetFile
 import de.robolab.common.planet.ServerPlanetInfo
-import de.robolab.server.net.RESTRequestClientCodeError
+import de.robolab.server.net.RESTResponseCodeException
 import de.robolab.server.externaljs.fs.*
 import de.robolab.server.externaljs.os.EOL
 import de.robolab.server.externaljs.path.safeJoinPath
@@ -109,7 +109,7 @@ class FilePlanetStore(val directory: String, val metaStore: IPlanetMetaStore) : 
     override suspend fun get(id: String): SPlanet? {
         var planetFile: PlanetFile? = null
         if (id.contains('\u0000'))
-            throw RESTRequestClientCodeError(
+            throw RESTResponseCodeException(
                 HttpStatusCode.UnprocessableEntity,
                 "Invalid id: \"${id.toIDString()}\""
             )
@@ -163,7 +163,7 @@ class FilePlanetStore(val directory: String, val metaStore: IPlanetMetaStore) : 
 
     private suspend fun lookupInfo(id: String): ServerPlanetInfo? {
         if (id.contains('\u0000'))
-            throw RESTRequestClientCodeError(
+            throw RESTResponseCodeException(
                 HttpStatusCode.UnprocessableEntity,
                 "Invalid id: \"${id.toIDString()}\""
             )

src/jsServerMain/kotlin/de/robolab/server/externaljs/express/EXPRESS.kt
@@ -5,7 +5,7 @@ package de.robolab.server.externaljs.express
 import de.robolab.common.net.HttpStatusCode
 import de.robolab.common.net.MIMEType
 import de.robolab.common.net.headers.ContentTypeHeader
-import de.robolab.server.net.RESTRequestClientCodeError
+import de.robolab.server.net.RESTResponseException
 import de.robolab.server.auth.User
 import de.robolab.server.externaljs.Buffer
 import de.robolab.server.externaljs.JSArray
@@ -15,6 +15,7 @@ import de.robolab.server.jsutils.JSDynErrorCallback
 import de.robolab.server.jsutils.PromiseScope
 import de.robolab.server.jsutils.jsCreateDelegate
 import de.robolab.server.jsutils.promise
+import de.robolab.server.net.RESTResponseCodeException
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
@@ -1043,10 +1044,11 @@ fun <ReqData, ResData, ReqDataT, ResDataT> Router<ReqDataT, ResDataT>.asMappedSu
 
 private fun handlePromiseError(err: Throwable, res: AnyResponse) {
     val (errorCode: HttpStatusCode, errorMessage: String?, errorMime: MIMEType?) = when (err) {
-        is RESTRequestClientCodeError -> Triple(err.code, err.message, err.mimeType)
+        is RESTResponseCodeException -> Triple(err.code, err.message, err.mimeType)
+        is RESTResponseException -> Triple(HttpStatusCode.BadRequest, err.message, err.mimeType)
         else -> Triple(HttpStatusCode.InternalServerError, err.message, null)
     }
-    if (err !is RESTRequestClientCodeError)
+    if (err !is RESTResponseException)
         console.error(err)
     if (!res.headersSent) {
         if (errorMime != null) {
@@ -1071,7 +1073,7 @@ private fun handlePromiseError(err: Throwable, res: AnyResponse) {
 private fun handlePromiseError(err: Throwable?, res: AnyResponse, next: (NodeError?) -> Unit) {
     when (err) {
         null -> next(err)
-        is RESTRequestClientCodeError -> handlePromiseError(err, res)
+        is RESTResponseException -> handlePromiseError(err, res)
         is NodeError -> next(err)
         else -> {
             handlePromiseError(err, res)
@@ -1083,10 +1085,10 @@ private fun handlePromiseError(err: Throwable?, res: AnyResponse, next: (NodeErr
 private fun <ReqData, ResData> TerminalPromiseCreator<ReqData, ResData>.toMiddleware(): Middleware<ReqData, ResData> {
     return { req, res, next ->
         var prom: Promise<*>? = null
-        var err: RESTRequestClientCodeError? = null
+        var err: RESTResponseException? = null
         try {
             prom = this(req, res)
-        } catch (ex: RESTRequestClientCodeError) {
+        } catch (ex: RESTResponseException) {
             err = ex
         }
         if (err != null && err != undefined)
@@ -1117,7 +1119,7 @@ private fun <ReqData, ResData> MiddlewarePromiseCreator<ReqData, ResData>.toMidd
         }
         try {
             prom = this(req, res, ::next2)
-        } catch (ex: RESTRequestClientCodeError) {
+        } catch (ex: RESTResponseException) {
             err = ex
         }
         if (err != null && err != undefined)

src/jsServerMain/kotlin/de/robolab/server/net/RESTRequestClientCodeError.kt
@@ -1,39 +1,0 @@
-package de.robolab.server.net
-
-import de.robolab.client.net.IServerResponse
-import de.robolab.client.net.requests.IRESTRequest
-import de.robolab.client.net.requests.IUnboundRESTRequest
-import de.robolab.common.net.HttpStatusCode
-import de.robolab.common.net.MIMEType
-import de.robolab.common.net.RESTRequestCodeError
-
-class RESTRequestClientCodeError : RESTRequestCodeError {
-    constructor(
-        code: HttpStatusCode,
-        message: String,
-        mimeType: MIMEType = MIMEType.PlainText,
-        triggeringRequest: IRESTRequest<*>? = null,
-        triggeringResponse: IServerResponse? = null
-    ) : super(code, message, mimeType, triggeringRequest, triggeringResponse)
-
-    constructor(
-        code: HttpStatusCode,
-        triggeringRequest: IRESTRequest<*>? = null,
-        triggeringResponse: IServerResponse? = null
-    ) : super(code, triggeringRequest, triggeringResponse)
-
-    constructor(
-        code: HttpStatusCode,
-        message: String,
-        mimeType: MIMEType = MIMEType.PlainText,
-        cause: Throwable?,
-        triggeringRequest: IRESTRequest<*>? = null,
-        triggeringResponse: IServerResponse? = null
-    ) : super(code, message, mimeType, cause, triggeringRequest, triggeringResponse)
-
-    constructor(
-        code: HttpStatusCode, cause: Throwable?,
-        triggeringRequest: IRESTRequest<*>? = null,
-        triggeringResponse: IServerResponse? = null
-    ) : super(code, cause, triggeringRequest, triggeringResponse)
-}

src/jsServerMain/kotlin/de/robolab/server/net/RESTResponseCodeException.kt
@@ -1,0 +1,37 @@
+package de.robolab.server.net
+
+import de.robolab.common.net.HttpStatusCode
+import de.robolab.common.net.MIMEType
+
+open class RESTResponseCodeException : RESTResponseException {
+    val code: HttpStatusCode
+
+    constructor(
+        code: HttpStatusCode,
+        message: String,
+        mimeType: MIMEType = MIMEType.PlainText
+    ) : super(message, mimeType) {
+        this.code = code
+    }
+
+    constructor(
+        code: HttpStatusCode
+    ) : super() {
+        this.code = code
+    }
+
+    constructor(
+        code: HttpStatusCode,
+        message: String,
+        mimeType: MIMEType = MIMEType.PlainText,
+        cause: Throwable?
+    ) : super(message, mimeType, cause) {
+        this.code = code
+    }
+
+    constructor(
+        code: HttpStatusCode, cause: Throwable?
+    ) : super(cause){
+        this.code = code
+    }
+}

src/jsServerMain/kotlin/de/robolab/server/net/RESTResponseException.kt
@@ -1,0 +1,34 @@
+package de.robolab.server.net
+
+import de.robolab.common.net.HttpStatusCode
+import de.robolab.common.net.MIMEType
+
+open class RESTResponseException : RuntimeException {
+
+    val mimeType: MIMEType?
+
+    constructor(
+        message: String,
+        mimeType: MIMEType = MIMEType.PlainText
+    ) : super(message) {
+        this.mimeType = mimeType
+    }
+
+    constructor() : super() {
+        this.mimeType = null
+    }
+
+    constructor(
+        message: String,
+        mimeType: MIMEType = MIMEType.PlainText,
+        cause: Throwable?
+    ) : super(message, cause) {
+        this.mimeType = mimeType
+    }
+
+    constructor(
+        cause: Throwable?
+    ) : super(cause) {
+        this.mimeType = null
+    }
+}

src/jsServerMain/kotlin/de/robolab/server/routes/AuthRouter.kt
@@ -3,7 +3,7 @@ package de.robolab.server.routes
 import de.robolab.common.net.HttpStatusCode
 import de.robolab.common.net.MIMEType
 import de.robolab.common.net.headers.AuthorizationHeader
-import de.robolab.server.net.RESTRequestClientCodeError
+import de.robolab.server.net.RESTResponseCodeException
 import de.robolab.server.auth.AuthService
 import de.robolab.server.auth.GitLabAuthProvider
 import de.robolab.server.auth.ShareCode
@@ -37,12 +37,12 @@ object AuthRouter {
             val dynCode: dynamic = req.query.code
             val dynState: dynamic = req.query.state
             val code: String? = dynCode as? String
-            val stateString: String = dynState as? String ?: throw RESTRequestClientCodeError(
+            val stateString: String = dynState as? String ?: throw RESTResponseCodeException(
                 HttpStatusCode.BadRequest,
                 "Missing State-parameter"
             )
             val shareCode: ShareCode
-            shareCode = authProvider.extractShareCode(stateString) ?: throw RESTRequestClientCodeError(
+            shareCode = authProvider.extractShareCode(stateString) ?: throw RESTResponseCodeException(
                 HttpStatusCode.BadRequest,
                 "State-parameter is not valid"
             )
@@ -89,11 +89,11 @@ window.close();
         }
         router.getSuspend("/gitlab/token") { req, res ->
             val dynState = req.query.state
-            val state = dynState as? String ?: throw RESTRequestClientCodeError(
+            val state = dynState as? String ?: throw RESTResponseCodeException(
                 HttpStatusCode.BadRequest,
                 "Missing State-parameter"
             )
-            val shareCode: ShareCode = authProvider.extractShareCode(state) ?: throw RESTRequestClientCodeError(
+            val shareCode: ShareCode = authProvider.extractShareCode(state) ?: throw RESTResponseCodeException(
                 HttpStatusCode.BadRequest,
                 "State-parameter is not valid"
             )

src/jsServerMain/kotlin/de/robolab/server/routes/PlanetRouter.kt
@@ -8,7 +8,7 @@ import de.robolab.common.net.MIMEType
 import de.robolab.server.config.Config
 import de.robolab.common.planet.ServerPlanetInfo
 import de.robolab.common.utils.map
-import de.robolab.server.net.RESTRequestClientCodeError
+import de.robolab.server.net.RESTResponseCodeException
 import de.robolab.server.data.*
 import de.robolab.server.externaljs.*
 import de.robolab.server.externaljs.express.*
@@ -132,7 +132,7 @@ object PlanetRouter {
         ) {
 
             fun throwIDNotFound(): Nothing {
-                throw RESTRequestClientCodeError(
+                throw RESTResponseCodeException(
                     HttpStatusCode.NotFound,
                     "Planet with id '${requestedID.toIDString()}' could not be found"
                 )

src/jvmMain/kotlin/de/robolab/client/ui/dialog/TokenDialog.kt
@@ -4,7 +4,7 @@ import de.robolab.client.net.IRobolabServer
 import de.robolab.client.net.requests.getTokenLinkPair
 import de.robolab.client.net.sendHttpRequest
 import de.robolab.common.net.HttpStatusCode
-import de.robolab.common.net.RESTRequestCodeError
+import de.robolab.common.net.RESTRequestCodeException
 import de.robolab.common.utils.Logger
 import javafx.scene.layout.Priority
 import kotlinx.coroutines.*
@@ -46,7 +46,7 @@ class TokenDialog : GenericDialog() {
                 while (isDocked) {
                     val r = tokenLinkPair.sendHttpRequest().also {
                         it.ifErr { e ->
-                            if (e !is RESTRequestCodeError || e.code != HttpStatusCode.NoContent) {
+                            if (e !is RESTRequestCodeException || e.code != HttpStatusCode.NoContent) {
                                 logger.warn(e.toString())
                             }
                         }