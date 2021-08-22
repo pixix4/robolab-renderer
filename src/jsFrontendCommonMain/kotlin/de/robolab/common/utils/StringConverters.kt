95c0d47506b21f3655d03e1383b2472e411f1f45
Alexander Schwerin <alexander_C._schwerin@web.de>
2021-08-22 12:32:32 +0200

    Fix (?) StringConverters name conflict

--- src/jsFrontendCommonMain/kotlin/de/robolab/common/utils/StringConverters.kt
+++ src/jsFrontendCommonMain/kotlin/de/robolab/common/utils/StringConverters.kt
@@ -1,21 +1,21 @@
 package de.robolab.common.utils
 
-private val btoa: (String) -> String = js("btoa").unsafeCast<(String) -> String>()
-private val atob: (String) -> String = js("atob").unsafeCast<(String) -> String>()
+private val btoa_js: (String) -> String = js("btoa").unsafeCast<(String) -> String>()
+private val atob_js: (String) -> String = js("atob").unsafeCast<(String) -> String>()
 
 
 actual fun String.encodeAsB64(url: Boolean): String {
-    return if (url) btoa(this).B64ToURLB64()
-    else btoa(this)
+    return if (url) btoa_js(this).B64ToURLB64()
+    else btoa_js(this)
 }
 
 actual fun String.decodeFromB64(url: Boolean): String {
-    return if (url) atob(this.URLB64ToB64())
-    else atob(this)
+    return if (url) atob_js(this.URLB64ToB64())
+    else atob_js(this)
 }
 
 actual fun String.decodeBytesFromB64(url: Boolean): ByteArray {
     if (url) return this.URLB64ToB64().decodeBytesFromB64(false)
-    val chars = atob(this).toCharArray()
+    val chars = atob_js(this).toCharArray()
     return ByteArray(chars.size) { chars[it].code.toByte() }
 }
